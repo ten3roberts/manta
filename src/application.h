075a1369df27392a369553aa884ac6da304f3477
ten3roberts <ten3roberts@gmail.com>
2020-01-16 16:36:34 +0100

    Started implementing vulkan

--- src/application.h
+++ src/application.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "event.h"
 
-
 int application_start();
 
 // The starting point from where all events should propagate
 void application_send_event(Event event);
+
+void* application_get_window();
