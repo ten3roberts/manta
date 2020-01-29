b488f4271c7b81738041573d96933a3bc57248b3
Tim Roberts <ten3roberts@gmail.com>
2020-01-29 17:04:34 +0100

    Added wall and execution time options to timer

--- src/timer.c
+++ src/timer.c
@@ -1,27 +1,70 @@
 #include "timer.h"
-
-Timer timer_start()
+#include "log.h"
+#if PL_WINDOWS
+#include <windows.h>
+#define GET_TICKS (GetTickCount64() * (CLOCKS_PER_SEC / 1000))
+#elif PL_LINUX
+#endif
+Timer timer_start(ClockType type)
 {
 	Timer timer;
-	timer.start = clock();
-	timer.end = 0;
+	timer.type = type;
+	if (type == CT_EXECUTION_TICKS)
+	{
+		timer.start_tick = clock();
+	}
+	else
+	{
+		timer.start_tick = GET_TICKS;
+	}
+	timer.end_tick = 0;
+
 	timer.running = 1;
+
 	return timer;
 }
 
 float timer_stop(Timer* timer)
 {
-	timer->end = clock();
-	timer->running = 0;
-	return (timer->end - timer->start) / (float)CLOCKS_PER_SEC;
+	if (timer->type == CT_EXECUTION_TICKS)
+	{
+		timer->end_tick = clock();
+		timer->running = 0;
+	}
+	else
+	{
+		timer->end_tick = GET_TICKS;
+	}
+	return (timer->end_tick - timer->start_tick) / (float)CLOCKS_PER_SEC;
 }
 
-float timer_duration(Timer* timer)
+void timer_reset(Timer* timer)
 {
-	return (timer->end - timer->start) / (float)CLOCKS_PER_SEC;
+	if (timer->type == CT_EXECUTION_TICKS)
+	{
+		timer->start_tick = clock();
+		timer->end_tick = 0;
+		timer->running = 1;
+	}
+	else
+	{
+		timer->start_tick = GET_TICKS;
+		timer->end_tick = 0;
+		timer->running = 1;
+	}
 }
 
-clock_t timer_ticks(Timer* timer)
+float timer_duration(const Timer* timer)
 {
-	return timer->end - timer->start;
+	if (timer->running == 0)
+		return (timer->end_tick - timer->start_tick) / (float)CLOCKS_PER_SEC;
+	if (timer->type == CT_EXECUTION_TICKS)
+		return (clock() - timer->start_tick) / (float)CLOCKS_PER_SEC;
+	// Wall time
+	return ((GET_TICKS)-timer->start_tick) / (float)CLOCKS_PER_SEC;
+}
+
+clock_t timer_ticks(const Timer* timer)
+{
+	return timer->end_tick - timer->start_tick;
 }
--- src/timer.h
+++ src/timer.h
@@ -1,20 +1,36 @@
 #include <time.h>
+#include <stdint.h>
+
+typedef enum
+{
+	// Measures the CPU execution time
+	CT_EXECUTION_TICKS, 
+	// Measures the monotonic wall clock
+	CT_WALL_TICKS
+} ClockType;
 
 typedef struct
 {
-	clock_t start;
-	clock_t end;
+	ClockType type;
+	uint64_t start_tick;
+	uint64_t end_tick;
 	short running;
 } Timer;
 
 // Starts and returns a timer
-Timer timer_start();
+Timer timer_start(ClockType type);
 
 // Stops a timer and returns the time passed
 float timer_stop(Timer* timer);
 
-// Reads the duration of a stopped timer
-float timer_duration(Timer* timer);
+// Resets the timer and starts it again if stopped
+// Can be used on a running clock
+void timer_reset(Timer* timer);
+
+// Reads the duration of a timer
+// If timer is stopped, it will read the time when the timer was stopped
+// If timer is running, it will read the current time elapsed
+float timer_duration(const Timer* timer);
 
 // Reads the duration of a stopped timer in CPU ticks
-clock_t timer_ticks(Timer* timer);
+clock_t timer_ticks(const Timer* timer);
