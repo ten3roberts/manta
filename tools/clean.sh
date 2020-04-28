5c4172ae67cdd338c5c8ea3b1b2da3089c9c951b
timmer <ten3roberts@gmail.com>
2020-04-28 16:25:21 +0200

    Added clean script

---
+++ tools/clean.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#This will clean the engine and included tools binary and intermediate files
+
+cd $(dirname $0)
+cd ..
+DIR=`pwd`
+
+echo "--------Cleaning crescent--------"
+
+# crescent
+rm -rv bin
+rm -rv obj
+
+# GLFW
+rm -rv vendor/glfw/bin
+rm -rv vendor/glfw/bin-int
+
+# compiled shaders
+rm -rv assets/shaders/*.spv
+
