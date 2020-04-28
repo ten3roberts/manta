1b6120f50fa3add3d1006b0f55813921b2670fd1
timmer <ten3roberts@gmail.com>
2020-04-28 16:18:00 +0200

    Included premake5 binary and made build script pick it if system binary doesn't exist

--- tools/build.sh
+++ tools/build.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-cd sandbox
-premake5 gmake2 && make || >&2 echo "Failed to build sandbox"
 
+# This script will build the engine and the included tools
+
+# Move to crescent folder to be agnostic of where the script is run
+cd $(dirname $0)
+cd ..
+DIR=`pwd`
+
+# Check if premake is installed, if not, use provided binary in vendor/premake
+command -v premake5 && echo "Using system `command -v premake5`" && premake="premake5" || echo "Using included premake5 binary" && premake="$DIR/vendor/premake/premake5"
+# Move into sandbox and attempt build
+
+cd sandbox
+echo "--------Entering sandbox--------"
+$premake gmake2 && make || >&2 echo "Failed to build sandbox"
+cd $DIR
